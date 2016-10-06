fce8d98fba5dff372edd5b724501d05adf55522c
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2016-10-06 20:58:43 +0800

    Add variable for homepage

--- Hw2/diabox.sh
+++ Hw2/diabox.sh
@@ -6,7 +6,15 @@ response=$?
 
 leave_msg="Sorry, you can't use nae browser if you don't like CTF."
 
+homepage="https://www.google.co.uk/" 
+
+# if the response is no or esc, give the leaving message
 if [ $response = 1 -o $response = 255 ] ; then
     dialog --title "Apology" --msgbox "$leave_msg" 20 100
+
+# if the response is yes, go to homepage
+elif [ $response = 0 ] ; then 
+    dialog --title "Nae browser" --msgbox "$(w3m -dump $homepage)" 200 100
+
 fi
 
