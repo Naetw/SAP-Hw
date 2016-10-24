261241e9cf2251e09da80aa448365003e7ac66cf
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2016-10-24 17:27:56 +0800

    Fix calculating dir number

--- Hw1/sahw2-1.sh
+++ Hw1/sahw2-1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-ls -ARl | sort -nr -k5,5 | awk 'BEGIN{dir=0;file=0;total=0}{if(/^\..*:$/)dir+=1 ; if((/^-.*/) && (NF > 8)){file+=1;total+=$5} ; if(NR < 6) print NR":"$5,$9} END {print "Dir num: " dir"\n""File num: " file"\n""Total: " total}'
+ls -ARl | sort -nr -k5,5 | awk 'BEGIN{dir=0;file=0;total=0}{if(/^d/)dir+=1 ; if((/^-.*/) && (NF > 8)){file+=1;total+=$5} ; if(NR < 6) print NR":"$5,$9} END {print "Dir num: " dir"\n""File num: " file"\n""Total: " total}'
