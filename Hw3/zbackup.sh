a980b9afda7f6e4e932abc3a2f57d15a59d1195a
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2016-11-02 14:54:50 +0800

    Add create func and fix list show field name

--- Hw3/zbackup.sh
+++ Hw3/zbackup.sh
@@ -8,7 +8,7 @@ if [ $# == 0 ]; then
 fi
 
 if [ $1 != "--list" -a $1 != "--delete" ]; then
-    #target=$1
+    target=$1
     
     # set rotate_count
     if [ "$2" == "" ]; then
@@ -16,12 +16,31 @@ if [ $1 != "--list" -a $1 != "--delete" ]; then
     else
         rotate_count=$(( $2-1 ))
     fi
-
+    
+    # set total number of this Dataset
+    total=$(zfs list -r -t snapshot | grep "^${target}@zbackup\..*" | awk -F '\n' 'END{print NR}')
+    echo $total
     # check snapshot number, if reach max, then delete oldest one (-q for no output)
-    zfs list -t snapshot | grep -q "^${target}@zbackup\.${rotate_count}"
+    zfs list -r -t snapshot | grep -q "^${target}@zbackup\.${rotate_count}"
     if [ $? == 0 ]; then
-        total=$(zfs list -t snapshot | grep "^${target}@zbackup\..*" | awk -F '\n' 'END{print NR}')
-    fi 
+        
+        # clean up 
+        while [ $total -gt $rotate_count ]; do
+            # delete the oldest one
+            zfs destroy -r ${target}@zbackup.`expr $total - 1`
+            total=$(( $total-1 ))
+        done
+    fi
+    
+    # rename the remaining list
+    while [ $total -gt 0 ]; do
+        src=$(( $total-1 ))
+        zfs rename -r ${target}@zbackup.${src} ${target}@zbackup.${total}
+        total=$(( $total-1 ))
+    done
+
+    # add new snapshot
+    zfs snapshot -r ${target}@zbackup.0
 elif [ $1 == "--list" ]; then
     
     # generate zbackup list
@@ -48,10 +67,21 @@ elif [ $1 == "--list" ]; then
         if [ "$3" != "" ]; then
             echo "$list" | awk -F '\n' -v spec_idx=$3 '{if(NR == spec_idx+1 || NR == 1)print $1;}' 
         else
-            echo "$list" | grep $2
+            echo "$list" | grep  -e $2 -e ID
         fi
     fi
 elif [ $1 == "--delete" ]; then
+    
+    # check dataset specified
+    if [ "$2" == "" ]; then
+        printf "\nYou need to specify the dataset!\n"
+        exit 1
+    fi
+    
+    #if [ "$3" == "" ]; then
+
+        # no ID specified - delete all
+        
 
 fi
 
