51b8b7fe8c4a262207086b6fbcb369a460ac9bf8
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2016-11-06 02:58:21 +0800

    Change to use stdin to upload file

--- Hw3/back2google.sh
+++ Hw3/back2google.sh
@@ -7,11 +7,13 @@ fi
 
 snapshot=$1
 
-filename=$(echo $1 | gawk '{match($1, /.*\/(.*)@.*/, dataset); print dataset[1]}')
+filename=$(echo $1 | gawk '{match($1, /(.*)@.*/, dataset); print dataset[1]}')
+filename=$(echo $filename | awk '{sub(/\//, "-", $1); print $1}')
+filename=$(echo $filename"@"`/bin/date "+%Y-%m-%d-%H-%M-%S"`)
 
 zfs send ${snapshot} | xz > $filename.xz
 openssl aes-256-cbc -in $filename.xz -out $filename.xz.enc
 
 size=$(ls -al $filename.xz.enc | awk '{print $5}')
 
-/usr/home/nae/gdrive upload ./$filename.xz.enc | pv -s $size
+pv -s $size $filename.xz.enc | /usr/home/nae/gdrive upload - ./$filename.xz.enc
