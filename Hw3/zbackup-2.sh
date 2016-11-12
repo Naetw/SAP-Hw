2a6e819b93d3a1549d4f9598af03bb2acb395a04
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2016-11-13 01:29:10 +0800

    Add rollback func and modify some variables

--- Hw3/zbackup-2.sh
+++ Hw3/zbackup-2.sh
@@ -2,7 +2,7 @@
 
 
 upload () {
-    snapshot=$2
+    snapshot=$1
 
     filename=$(echo $snapshot | gawk '{match($1, /(.*)@.*/, dataset); print dataset[1]}')
     filename=$(echo $filename | awk '{sub(/\//, "-", $1); print $1}')
@@ -14,16 +14,22 @@ upload () {
     size=$(ls -al $filename.xz.enc | awk '{print $5}')
 
     pv -s $size $filename.xz.enc | /usr/home/nae/gdrive upload - $filename.xz.enc
+    
+    # delete file
+    rm -f $filename.xz 
+    rm -f $filename.xz.enc
 }
 
 rollback () {
-    fileID=$2
+    fileID=$1
 
     # get file name
-    infile=$(/usr/home/nae/gdrive info $fileID | grep Name)
-    infile=$(echo "$infile" | gawk  -F '\n' '{match($1, /.* (.*)/, name); print name[1]}')
-    outfile=$(echo "$infile" | gawk -F '\n' '{match($1, /(.*)\.enc/, name); print name[1]}')
-
+    filename=$(/usr/home/nae/gdrive info $fileID | grep Name)
+    filename=$(echo "$filename" | gawk  -F '\n' '{match($1, /.* (.*)\.xz\.enc/, name); print name[1]}')
+    
+    # get dataset
+    dataset=$(echo "$filename" | gawk -F '\n' '{match($1, /(.*)@.*/, name); print name[1]}')
+    dataset=$(echo "$dataset" | gawk -F '\n' '{sub(/-/, "/", $1); print $1}')
 
     # grep size info for ETA in pv
     size=$(/usr/home/nae/gdrive info --bytes  $fileID | grep Size)
@@ -31,18 +37,32 @@ rollback () {
 
     printf "Downloading....\n"
     # --stdout for pv to show progress, --no-progress for disabling gdrive progress info, redirect stdout garbage to null
-    /usr/home/nae/gdrive download $fileID --stdout --no-progress | pv -s $size 1>${infile}
+    /usr/home/nae/gdrive download $fileID --delete --stdout --no-progress | pv -s $size 1>$filename.xz.enc
 
     printf "\nDone!\n\n"
 
+    openssl aes-256-cbc -d -in $filename.xz.enc -out $filename.xz
+    xz -fd $filename.xz 
+    
+    # get snapshot namei and symbol
+    snapshot=$(file $filename | gawk -F '\n' '{match($1, /.*name: .(.*)./, name); print name[1]}')
+    symbol=$(echo "$snapshot" | gawk -F '\n' '{match($1, /.*@(.*)/, name); print name[1]}')
+
     printf "Rollback....\n"
-    openssl aes-256-cbc -d -in $infile -out $outfile
-    xz -fd $outfile 
+
+    echo $dataset
+    zfs recv ${dataset}-zbackup@${symbol} < $filename
+    zfs rename ${dataset} ${dataset}-old
+    zfs rename ${dataset}-zbackup ${dataset}
+    zfs destroy -r ${dataset}-old
+
+    # delete file
+    rm -rf $filename.xz.enc 
 }
 
 if [ "$1" == "--upload" ]; then
-    upload
+    upload $2
 elif [ "$1" == "--rollback" ]; then
-    rollback
+    rollback $2 
 fi
 
